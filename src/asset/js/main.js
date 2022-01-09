fb05e58f6dbca727fb1f4378f3ada98dc7ca82fe
Ali <97373457+DevNull-IR@users.noreply.github.com>
2022-01-09 10:47:01 +0330

    Create main.js

---
+++ src/asset/js/main.js
@@ -0,0 +1,22 @@
+let links = document.getElementsByTagName("url");
+for (let i = 0; i < links.length; i++) {
+let link = document.getElementsByTagName("url")[i];
+let get = link.getAttribute('target');
+let attr = link.getAttribute('link');
+link.addEventListener('click', ()=>{
+if (get == "_blank"){
+   window.open(attr, '_blank');
+   } else {
+   if (get == "_self"){
+   window.open(attr, '_self');
+   }
+   if (get == "_parent"){
+   window.open(attr, '_parent');
+   }
+   if (get == "_top"){
+   window.open(attr, '_top');
+   }
+   }
+})
+
+}
